6875f80a2e2a0ab4364c677b74adfe33a1f6495e
Sunga Thawethe <sungabt@gmail.com>
2023-09-21 08:28:26 +0200

    Create Like MOdel

---
+++ app/models/like.rb
@@ -0,0 +1,25 @@
+class Like < ApplicationRecord
+  
+  # Associations
+
+  belongs_to :user
+  belongs_to :post
+
+
+  # Callbacks
+
+  after_create :increment_post_likes_counter
+  after_destroy :decrement_post_likes_counter
+
+  # Methods
+
+  def increment_post_likes_counter
+    post.increment!(:likes_counter)
+  end
+
+  def decrement_post_likes_counter
+    post.decrement!(:likes_counter)
+
+  end
+
+end
