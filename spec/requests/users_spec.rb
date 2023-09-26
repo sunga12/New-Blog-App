8ef1f5dc9027263635224023f20e6e35929bb1fa
Sunga Thawethe <sungabt@gmail.com>
2023-09-26 17:26:31 +0200

    Fix Linter errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,38 +2,36 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    before { get '/users#index' }
 
-    before { get "/users#index" }
-
-    it "returns http success" do
-      expect(response.status).to eq(200)  
+    it 'returns http success' do
+      expect(response.status).to eq(200)
     end
 
-    it "renders the right action" do
+    it 'renders the right action' do
       expect(response).to render_template('index')
     end
 
-    it "contains the coreect placeholder text" do
+    it 'contains the coreect placeholder text' do
       expect(response.body).to include('Here is a list of users')
     end
   end
 
   describe 'GET /show' do
-
     before do
       user = 123
       get user_path(user)
     end
 
-    it "returns http success" do
-      expect(response.status).to eq(200)  
+    it 'returns http success' do
+      expect(response.status).to eq(200)
     end
 
-    it "renders the right action" do
+    it 'renders the right action' do
       expect(response).to render_template('show')
     end
 
-    it "renders the right action" do
+    it 'renders the right action' do
       expect(response.body).to include('Here is a given user')
     end
   end
