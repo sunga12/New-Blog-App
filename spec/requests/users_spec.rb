fd39fc379feccc4838e6c1a9c5299d0efbb07275
Sunga Thawethe <sungabt@gmail.com>
2023-09-26 17:23:28 +0200

    Write Controller Specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,39 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    before { get "/users#index" }
+
+    it "returns http success" do
+      expect(response.status).to eq(200)  
+    end
+
+    it "renders the right action" do
+      expect(response).to render_template('index')
+    end
+
+    it "contains the coreect placeholder text" do
+      expect(response.body).to include('Here is a list of users')
+    end
+  end
+
+  describe 'GET /show' do
+
+    before do
+      user = 123
+      get user_path(user)
+    end
+
+    it "returns http success" do
+      expect(response.status).to eq(200)  
+    end
+
+    it "renders the right action" do
+      expect(response).to render_template('show')
+    end
+
+    it "renders the right action" do
+      expect(response.body).to include('Here is a given user')
+    end
   end
 end
