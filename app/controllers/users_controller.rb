ef63e86ba7b0e4e2de6f9f4e275aa8fa9553a24a
Sunga Thawethe <sungabt@gmail.com>
2023-09-27 12:41:16 +0200

    Add Users Views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+
+  def initialize
+  @users = User.all
+  end
+  
   def index; end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
