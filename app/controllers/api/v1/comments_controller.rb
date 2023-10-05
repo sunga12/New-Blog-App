051a0dd07b5d3080ad50c3d02f2905782705ac3b
Aamir <aamirasaram2107@gmail.com>
2023-10-05 17:20:34 +0200

    Add API endpoint for creating comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,4 +4,16 @@ class Api::V1::CommentsController < ApplicationController
     @comments = @post.comments
     render json: @comments
   end
+
+  def create
+    @comment = Comment.new(params.require(:comment).permit(:text))
+    @comment.post = Post.find(params[:post_id])
+    @comment.author = current_user
+
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: { comment: @comment.errors, status: :unprocessable_entity }
+    end
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,12 +16,6 @@ class CommentsController < ApplicationController
     @comment = Comment.new(params.require(:comment).permit(:text))
     @comment.post = Post.find(params[:post_id])
     @comment.author = current_user
-
-    if @comment.save
-      render json: @comment, status: :created, location: @comment
-    else
-      render json: { comment: @comment.errors, status: :unprocessable_entity }
-    end
   end
 
   def destroy
